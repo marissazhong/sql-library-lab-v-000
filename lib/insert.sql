4ff7ba4ccfa47143bab036c5e5e82b1c943aaac7
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 14:04:30 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,12 @@
 
-INSERT INTO characters VALUES (1, "Alpha", "I am first", "human", 1, 1);
-INSERT INTO characters VALUES (1, "Beta", "I am second", "human", 1, 1);
-
+INSERT INTO characters VALUES (1, "Alpha", "I am first", "God", 1, 1);
+INSERT INTO characters VALUES (2, "Beta", "I am second", "God", 1, 1);
+INSERT INTO characters VALUES (3, "Gamma", "I am third", "Demi-god", 1, 1);
+INSERT INTO characters VALUES (4, "Delta", "I am fourth", "Demi-god", 1, 1);
+INSERT INTO characters VALUES (1, "Alpha", "I am first", "God", 1, 2);
+INSERT INTO characters VALUES (2, "Beta", "I am second", "God", 1, 2);
+INSERT INTO characters VALUES (3, "Gamma", "I am third", "Demi-god", 1, 2);
+INSERT INTO characters VALUES (4, "Delta", "I am fourth", "Demi-god", 1, 2);
 
 INSERT INTO books VALUES (1, "My Book 1.1", "1900", 1);
 INSERT INTO books VALUES (2, "My Book 1.2", "1900", 1);
@@ -19,4 +24,5 @@ INSERT INTO authors VALUES (2, "Author 2");
 INSERT INTO subgenres VALUES (1, "Subgenre 1");
 INSERT INTO subgenres VALUES (2, "Subgenre 2");
 
+"character_id, book_id"
 INSERT INTO characters_books VALUES (1, 1, 1);
