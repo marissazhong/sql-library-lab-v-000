29442943f774ff459cb4d40c4f51e5d57513ba63
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 13:53:43 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 
 
 INSERT INTO characters VALUES (1, "Character name", "my motto", "human", 1, 1);
 INSERT INTO books VALUES (1, "Title", "1900", 1);
-INSERT INTO series VALUES (1, "Title", 1, 1);
+INSERT INTO series VALUES (1, "Name");
