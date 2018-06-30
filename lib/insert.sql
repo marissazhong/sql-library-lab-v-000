5fcd608889eab33a037b8e436f40c3f7c0d07469
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 13:59:05 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,9 +6,10 @@ INSERT INTO books VALUES (1, "Title", "1900", 1);
 INSERT INTO series VALUES (1, "Series 1", 1, 1);
 INSERT INTO series VALUES (2, "Series 2", 2, 2);
 
-INSERT INTO authors VALUES (1, "name");
+INSERT INTO authors VALUES (1, "Author 1");
+INSERT INTO authors VALUES (2, "Author 2");
 
 INSERT INTO subgenres VALUES (1, "Subgenre 1");
-INSERT INTO subgenres VALUES (1, "Subgenre 2");
+INSERT INTO subgenres VALUES (2, "Subgenre 2");
 
 INSERT INTO characters_books VALUES (1, 1, 1);
