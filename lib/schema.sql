cdb62ff7c87ad9411a001334a9bb7c63c778a9e0
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 13:52:53 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 
 
 INSERT INTO characters VALUES (1, "Character name", "my motto", "human", 1, 1);
