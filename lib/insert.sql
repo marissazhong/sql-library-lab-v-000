83c7484948a0d4faf91a5075575a271e892d6d24
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 13:58:09 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO characters VALUES (1, "Character name", "my motto", "human", 1, 1);
+
+INSERT INTO books VALUES (1, "Title", "1900", 1);
+
+INSERT INTO series VALUES (1, "Series 1", 1, 1);
+INSERT INTO series VALUES (2, "Series 2", 1, 1);
+
+INSERT INTO authors VALUES (1, "name");
+
+INSERT INTO subgenres VALUES (1, "name");
+
+INSERT INTO characters_books VALUES (1, 1, 1);
