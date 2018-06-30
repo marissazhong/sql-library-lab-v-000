ede01cc86e5bd224bf4e156271d552e5bd0a1c14
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 14:00:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,10 +7,6 @@ INSERT INTO books VALUES (3, "My Book 1.3", "1900", 1);
 INSERT INTO books VALUES (4, "My Book 2.1", "1950", 2);
 INSERT INTO books VALUES (5, "My Book 2.2", "1950", 2);
 INSERT INTO books VALUES (6, "My Book 2.3", "1950", 2);
-INSERT INTO books VALUES (7, "My Book 3.1", "2000", 3);
-INSERT INTO books VALUES (8, "My Book 3.2", "2000", 3);
-INSERT INTO books VALUES (9, "My Book 3.3", "2000", 3);
-
 
 INSERT INTO series VALUES (1, "Series 1", 1, 1);
 INSERT INTO series VALUES (2, "Series 2", 2, 2);
