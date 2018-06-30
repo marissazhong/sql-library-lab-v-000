985929a7aa078a19d75348523768ca7b7c1ae672
Marissa Zhong <mwz2101@columbia.edu>
2018-06-30 13:51:15 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,3 @@
-CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT);
- 
-INSERT INTO characters VALUES (1, "Apples", 2, 1);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+
+INSERT INTO characters VALUES (1, "Character name", "my motto", "human", 1, 1);
